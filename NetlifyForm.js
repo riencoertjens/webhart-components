fd952b46be0741ba895172c21340e4a36eaf26a9
Rien <rien.coertjens@gmail.com>
2019-01-22 19:57:31 +0100

    Update NetlifyForm.js

--- NetlifyForm.js
+++ NetlifyForm.js
@@ -9,10 +9,12 @@ class NetlifyForm extends Component {
   handleSubmit = e => {
     e.preventDefault()
     const formData = new FormData(e.target)
+    formData.append('no-cache', true)
+
     for (var pair of formData.entries()) {
       console.log(pair[0] + ', ' + pair[1])
     }
-    fetch('/?no-cache=1', {
+    fetch('/', {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       body: formData,
